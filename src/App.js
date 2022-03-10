74a3088ad98a4a12f82186678cf1e4812170a299
Mohamed-84Taher <benarbiamohamed84@gmail.com>
2022-03-10 16:38:52 +0100

    call seif

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Seif from "./Seif";
 
 function App() {
   return (
     <div className='App'>
       <h1>Ws Git</h1>
+      <Seif />
     </div>
   );
 }
