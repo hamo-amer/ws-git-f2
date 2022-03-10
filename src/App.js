22888cc892df4515aaf1b4adc4f7a0ca51ffb76e
Mohamed-84Taher <benarbiamohamed84@gmail.com>
2022-03-10 16:41:45 +0100

    call mohamed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Mohamed from "./Mohamed";
 
 function App() {
   return (
     <div className='App'>
       <h1>Ws Git</h1>
+      <Mohamed />
     </div>
   );
 }
